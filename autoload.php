e04d7f0e0e481dd71bb6e73e18c96969b68e1599
xielei <443833189@qq.com>
2017-11-22 13:54:15 +0800

    //配置老客户端

---
+++ autoload.php
@@ -0,0 +1,11 @@
+<?php
+
+function classLoader($class) {
+    $path = str_replace(['\\', 'Iris/SphinxSearch/'], [DIRECTORY_SEPARATOR, ''], $class);
+    $file = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR .'SphinxSearch'.DIRECTORY_SEPARATOR. $path . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
+
+spl_autoload_register('classLoader');
